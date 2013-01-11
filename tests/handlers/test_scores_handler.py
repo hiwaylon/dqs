b22fd3d71f657f0e2392cece4021d4e4fbaa75dd
Waylon Calabrese <hiwaylon@gmail.com>
2013-01-11 10:33:27 -0600

    switching to flask for prototyping

---
+++ tests/handlers/test_scores_handler.py
@@ -0,0 +1,19 @@
+"""Integration tests for handling ``Video`` resource requests."""
+
+import unittest
+
+#
+# TODO: Mongo connection
+#
+
+
+class BaseTest(unittest.TestCase):
+    pass
+
+
+class TestScoresHandler(BaseTest):
+    def setUp(self):
+        super(TestScoresHandler, self).setUp()
+
+    def test_(self):
+        self.fail()
